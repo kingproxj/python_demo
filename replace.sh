295a6715fe0e77cfddfd1512cedc30ba95b7aeb5
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-12 15:55:29 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,10 +1,10 @@
 myhandler=$(echo $Handler | cut -d"." -f1)
 myfunction=$(echo $Handler | cut -d"." -f2)
 echo $myhandler $myfunction
-handlerstr=$(echo 's/HandlerName/'$myhandler'/')
+handlerstr=$(echo 's/HandlerName/'$myhandler'/g')
 echo $handlerstr
 sed -i $handlerstr server.py
 
-functionstr=$(echo 's/FunctionName/'$myfunction'/')
+functionstr=$(echo 's/FunctionName/'$myfunction'/g')
 echo $functionstr
 sed -i $functionstr fcs_serverless_index.py

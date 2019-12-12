ac2373804dc5c324315f34dd1fad3d01f2e5c19f
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-12 16:39:24 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -3,8 +3,8 @@ myfunction=$(echo $Handler | cut -d"." -f2)
 echo $myhandler $myfunction
 handlerstr=$(echo 's/HandlerName/'$myhandler'/g')
 echo $handlerstr
-sed -i $handlerstr fcs_serverless_index.py
+sed -i $handlerstr __fcs_serverless_index__.py
 
 functionstr=$(echo 's/FunctionName/'$myfunction'/g')
 echo $functionstr
-sed -i $functionstr fcs_serverless_index.py
+sed -i $functionstr __fcs_serverless_index__.py

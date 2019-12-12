191587d253281a9e5daf0184e35a20fb95a99d87
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-12 15:40:32 +0800

    Update fcs_serverless_index.py

--- fcs_serverless_index.py
+++ fcs_serverless_index.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import index
+import HandlerName
 
 def application(environ, start_response):
     entname = '山东惠硕信息技术有限公司'
     #result = calc_main_feature(entname)
     print("environ['QUERY_STRING']")
-    result = index.application(environ, start_response)
+    result = HandlerName.FunctionName(environ, start_response)
     return result

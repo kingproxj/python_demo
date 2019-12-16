d3219b6bfd37851c8daf6753c500453bdbb74564
kingproxj <48118067+kingproxj@users.noreply.github.com>
2019-12-16 14:00:10 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,10 @@ import os
 
 from multiprocessing import Process
 import time
+# 从wsgiref模块导入:
+from wsgiref.simple_server import make_server
+# 导入我们自己编写的application函数:
+from __fcs_serverless_index__ import application
 
 #os.environ["Handler"]="index.application"
 #os.environ['CodeUri'] = 'https://raw.githubusercontent.com/kingproxj/python_demo/master/replace.sh,https://raw.githubusercontent.com/kingproxj/python_demo/start.sh'
@@ -41,5 +45,11 @@ print("开始执行replace.sh")
 repval = os.system('sh replace.sh')
 print("执行结果为",repval)
 if repval == 0: 
-    print("开始执行server.py") #启动服务
-    os.system('python3 {}'.format("server.py"))
+    #print("开始执行server.py") #启动服务
+    #os.system('python3 {}'.format("server.py"))
+
+    # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+    httpd = make_server('', 8000, application)
+    print('Serving HTTP on port 8000...')
+    # 开始监听HTTP请求:
+    httpd.serve_forever()

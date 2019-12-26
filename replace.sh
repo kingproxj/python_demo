8e390a6596577b8962a615fb9c25f407f2015d3b
xujun <xujun@inspur.com>
2019-12-26 20:51:52 +0800

    下载文件

--- replace.sh
+++ replace.sh
@@ -1,12 +1,7 @@
 myhandler=$(echo $Handler | cut -d"." -f1)
 myfunction=$(echo $Handler | cut -d"." -f2)
 echo $myhandler $myfunction
-handlerFile=$myhandler'.py'
-echo 'def '$myfunction'(environ, start_response):' > $handlerFile
-echo '    result="test" ' >> $handlerFile
-echo '    responsebody = str(result) ' >> $handlerFile
-echo '    start_response("200 OK", [("Content-Type","application/json")]) ' >> $handlerFile
-echo '    return [bytes(responsebody, encoding = "utf8")] ' >> $handlerFile
+
 handlerstr=$(echo 's/HandlerName/'$myhandler'/g')
 echo $handlerstr
 sed -i $handlerstr __fcs_serverless_index__.py
